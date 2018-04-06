bb5959783604831124074270b1107d9a98fb3d07
Akira Ota 10nin <akira19870221@gmail.com>
2018-04-06 20:06:31 +0900

    Add get title function.

--- src/gobrin/core.clj
+++ src/gobrin/core.clj
@@ -2,4 +2,9 @@
   (:require [net.cgrand.enlive-html :as html]))
 
 (defn fetch-url [url]
+  "get xml resrouce from url."
   (html/xml-resource (java.net.URL. url)))
+
+(defn get-title [res]
+  "get <title> list from resource."
+  (map html/text (html/select res [:title])))
