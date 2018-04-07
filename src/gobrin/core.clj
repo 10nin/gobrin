ccfee9e85f920bffb7065ec9b9aedd19dec4bd5c
Akira Ota 10nin <akira19870221@gmail.com>
2018-04-08 03:05:11 +0900

    Fixed function behavior.
    Fixed get-title without channel header's title.
    Fixed head function append '...' for long title.

--- src/gobrin/core.clj
+++ src/gobrin/core.clj
@@ -7,7 +7,9 @@
 
 (defn get-title [res]
   "get <title> list from resource."
-  (map html/text (html/select res [:title])))
+  (map html/text (html/select res [:item :title])))
 
 (defn head [n s]
-  (apply str (take n s)))
+  (let [ss (apply str (take n s))]
+    (if (< n (count s)) (str ss "...")
+        ss)))
