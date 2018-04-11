d4d4ef566628fe98afe2004ce32dba4ccda60c4c
Akira Ota 10nin <akira19870221@gmail.com>
2018-04-11 20:09:44 +0900

    Add title mapping function and hyperlink joint function.

--- src/gobrin/core.clj
+++ src/gobrin/core.clj
@@ -6,9 +6,9 @@
   "get xml resrouce from url."
   (html/xml-resource (java.net.URL. url)))
 
-(defn make-title-map [title-elements]
-  (let [l (first title-elements)
-        t (second title-elements)]
+(defn make-title-map- [elm]
+  (let [l (first elm)
+        t (second elm)]
     {(:tag l)
      (->> (:content l)
           first),
@@ -21,11 +21,22 @@
   "get <link> and <title> list from resource."
   (partition 2 (html/select res [:item :> #{:title :link}])))
 
+(defn make-title-map [title-elements]
+  (map make-title-map- title-elements))
+
 (defn head [n s]
   (let [ss (apply str (take n s))]
     (if (< n (count s)) (str ss "...")
         ss)))
 
-(defn make-hyperlink [dic]
+(defn make-hyperlink- [dic]
   "Make <a> tag link from {:text 'caption', :link url} style dictionary."
   (tmpl/render "<a href=\"{{link}}\" target=\"_blank\">{{title}}</a>" dic))
+
+(defn make-hyperlink [url]
+  (map
+   make-hyperlink
+   (-> (fetch-url url)
+       get-title-elements
+       make-title-map)))
+      
