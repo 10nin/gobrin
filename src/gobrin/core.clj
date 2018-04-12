1d942cef7e88ecb46d8c3697c917ef0f51c6e720
Akira Ota 10nin <akira19870221@gmail.com>
2018-04-12 21:32:02 +0900

    Add mapping div tag function and refactoring function names.

--- src/gobrin/core.clj
+++ src/gobrin/core.clj
@@ -6,6 +6,11 @@
   "get xml resrouce from url."
   (html/xml-resource (java.net.URL. url)))
 
+(defn head [n s]
+  (let [ss (apply str (take n s))]
+    (if (< n (count s)) (str ss "...")
+        ss)))
+
 (defn make-title-map- [elm]
   (let [l (first elm)
         t (second elm)]
@@ -24,18 +29,20 @@
 (defn make-title-map [title-elements]
   (map make-title-map- title-elements))
 
-(defn head [n s]
-  (let [ss (apply str (take n s))]
-    (if (< n (count s)) (str ss "...")
-        ss)))
-
 (defn make-hyperlink- [dic]
   "Make <a> tag link from {:text 'caption', :link url} style dictionary."
   (tmpl/render "<a href=\"{{link}}\" target=\"_blank\">{{title}}</a>" dic))
 
+(defn make-div [id links]
+  (tmpl/render "<div id=\"{{id}}\">
+{% for l in links %}
+{{l|safe}}<br>
+{% endfor %}
+</div>" {:id id, :links links}))
+
 (defn make-hyperlink [url]
   (map
-   make-hyperlink
+   make-hyperlink-
    (-> (fetch-url url)
        get-title-elements
        make-title-map)))
