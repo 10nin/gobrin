0af42c83d52e76826de3fe5a54c0f27a49e3d3ba
Akira Ota 10nin <akira19870221@gmail.com>
2018-04-18 19:48:04 +0900

    Add server handlers.

--- src/gobrin/core.clj
+++ src/gobrin/core.clj
@@ -1,8 +1,27 @@
 (ns gobrin.core
-  (:require [net.cgrand.enlive-html :as html]
+  (:require [compojure.core :refer [defroutes context GET]]
+            [compojure.route :as route]
+            [ring.adapter.jetty :as server]
+            [ring.util.response :as res]
+            [net.cgrand.enlive-html :as html]
             [selmer.parser :as tmpl]))
 
 (def ^:dynamic *rss-list* '({:id "kantei" :rss "https://www.kantei.go.jp/index-jnews.rdf"}))
+(defonce server (atom nil))
+(defn html [res]
+  (assoc res :headers {"Content-Type" "text/html; charset=utf-8"}))
+
+(defn root-view [req]
+  "<p>Soon...</p>")
+
+(defn root-handler [req]
+  (-> (root-view req)
+      res/response
+      html))
+
+(defroutes handlers
+  (GET "/" req root-handler)
+  (route/not-found "<h1>HTTP 404 : Not found</h1>"))
 
 (defn fetch-url [url]
   "get xml resrouce from url."
@@ -55,3 +74,19 @@
 (defn render-html [url-list]
   (let [contents (map #(make-hyperlink (:rss %)) url-list)]
     (map render-html- contents)))
+
+(defn start-server [& {:keys [host port join?]
+                       :or {host "localhost" port 3000 join? false}}]
+  (let [port (if (string? port) (Integer/parseInt port) port)]
+    (when-not @server
+      (reset! server (server/run-jetty #'handlers {:host host :port port :join? join?})))))
+
+(defn stop-server []
+  (when @server
+    (.stop @server)
+    (reset! server nil)))
+
+(defn restart-server []
+  (when @server
+    (stop-server)
+    (start-server)))
